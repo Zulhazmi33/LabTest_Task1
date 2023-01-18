85b6cca9cd3ec35139fe30c8c7559c4b50741174
Zulhazmi33 <hazmi1ab@gmail.com>
2023-01-18 17:05:14 +0800

    Task 2

--- app/src/main/java/com/example/mindsharpener/MainActivity.kt
+++ app/src/main/java/com/example/mindsharpener/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mindsharpener
 
-import android.support.v7.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun difficulty(){
+            val textView = findViewById<View>(R.id.view4) as TextView
+            textView.setText("3")
+        }
+
     }
 }
